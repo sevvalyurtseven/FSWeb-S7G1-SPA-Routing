8b6b080ac7408255d0bf6f219cc0035114485ebf
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-21 21:07:46 +0300

    FilmCard componenti olusturuldu

--- client/src/Filmler/FilmCard.js
+++ client/src/Filmler/FilmCard.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function FilmCard (props) {
-  return;
+export default function FilmCard(props) {
+  const { title, director, metascore, id } = props.movie;
+
+  return (
+    <Link to={`/filmler/${id}`}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+      </div>
+    </Link>
+  );
 }
--- client/src/Filmler/FilmListesi.js
+++ client/src/Filmler/FilmListesi.js
@@ -1,30 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import FilmCard from "./FilmCard";
 
 export default function FilmListesi(props) {
   return (
     <div className="movie-list">
       {props.movies.map((movie) => (
-        <FilmDetayları key={movie.id} movie={movie} />
+        <FilmCard key={movie.id} movie={movie} />
       ))}
     </div>
-  );
-}
-
-function FilmDetayları(props) {
-  const { title, director, metascore, id } = props.movie;
-
-  return (
-    <Link to={`/filmler/${id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-      </div>
-    </Link>
   );
 }
