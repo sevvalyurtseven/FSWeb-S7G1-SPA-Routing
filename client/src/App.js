8ee21d0bd5a8fa9fff2a485bc0373963fd908572
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-04-21 21:00:47 +0300

    ayni filmin 2. kez kaydedilmesini engelledik

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,10 @@ export default function App() {
   }, []);
 
   const KaydedilenlerListesineEkle = (movie) => {
-    setSaved([...saved, movie]);
+    const film = saved.find((savedMovie) => savedMovie.id === movie.id);
+    if (!film) {
+      setSaved([...saved, movie]);
+    }
     // Burası esnek. Aynı filmin birden fazla kez "saved" e eklenmesini engelleyin
   };
 
